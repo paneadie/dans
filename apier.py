dfc10760810d0beb1e3b8a8156ccc6041067b138
stu <stuart@vandegiessen.net>
2021-08-11 23:13:50 +1000

    Finally have a clean working setup for the additional details!

---
+++ apier.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import pickle
+import requests
+import numpy as np
+import pandas as pd
+import time
+import csv
+import re
+import json
+import xlrd
+import os
+from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime, date, timedelta
+import nltk
+from selenium.webdriver.firefox.options import Options
+#from flatten_json import flatten
+import time
+
+import glob
+
+# Here we'll loop through all files and blend them into one.
+
+path = r'/home/stu/code/dans' # use your path
+all_files = glob.glob(path + "/scraped*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+
+# And now to cleanup results, and remove duplicates
+
+wines = frame
+# And now I'll filter out the product linkes
+wines = wines[wines.URL.str.contains('https://www.danmurphys.com.au/product')]
+
+wines = wines.replace(r'https://www.danmurphys.com.au/product/DM_', r'', regex=True)
+wines = wines.replace(r'/.*$', r'', regex=True)
+wines = wines.drop_duplicates()
+
+
+#And now to call the API
+
+#wines
+#mylist = df['Cleaned'].tolist()
+mylist = wines['URL'].tolist()
+
+#ylist = mylist[0:100]
+result = []
+
+for wine in mylist:
+    url = "https://api.danmurphys.com.au/apis/ui/Product/" + str(wine)
+    r = requests.get(url).json()
+    try:
+        rtoo = pd.json_normalize(r["Products"])
+        result.append(rtoo)
+    except:
+        print("No product found")
+
+my_df = pd.concat(result)
+file_res = "API_results_" + time.strftime("%Y%m%d") + ".csv"
+my_df.to_csv(file_res)
+#pd.json_normalize(flatten(r))
+
---
+++ scraper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import pickle
+import requests
+import numpy as np
+import pandas as pd
+import time
+import csv
+import re
+import json
+import xlrd
+import os
+from selenium.common.exceptions import NoSuchElementException
+from datetime import datetime, date, timedelta
+import nltk
+from selenium.webdriver.firefox.options import Options
+#from flatten_json import flatten
+regex = re.compile(r"\[|\]|<", re.IGNORECASE)
+
+import time
+
+### Dans
+#make headless
+options = Options()
+options.headless = True
+
+driver = webdriver.Firefox(options=options,executable_path=r'/usr/local/bin/geckodriver')
+path = "https://www.danmurphys.com.au/red-wine/all"
+driver.get(path)   
+
+file = "scraped_" + time.strftime("%Y%m%d") + ".csv"
+
+
+with open(file, "w") as csvFile:
+    fieldnames = ['URL']
+    writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+    writer.writeheader()
+    for item in range(1,500):
+        path = "https://www.danmurphys.com.au/red-wine/all?page=" + str(item)
+        driver.implicitly_wait(2)
+        driver.get(path)
+        elems = driver.find_elements_by_xpath("//a[@href]")
+        for elem in elems:
+            writer.writerow({'URL': elem.get_attribute("href")})
+        print("Page done")
+
+
+
